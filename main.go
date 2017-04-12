e4e8b9a5689173d13345d21a75f4e415ccfb5a67
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-04-12 15:09:31 +0900

    simplify

--- main.go
+++ main.go
@@ -20,13 +20,13 @@ func main() {
 	name := gitconfig("user.name")
 	email := gitconfig("user.email")
 	cmd := exec.Command("git", "filter-branch", "-f", "--env-filter",
-		fmt.Sprintf(strings.Join([]string{
-			"GIT_AUTHOR_NAME='%[1]s';",
-			"GIT_AUTHOR_EMAIL='%[2]s';",
-			"GIT_COMMITTER_NAME='%[1]s';",
-			"GIT_COMMITTER_EMAIL='%[2]s';",
+		fmt.Sprintf(""+
+			"GIT_AUTHOR_NAME='%[1]s';"+
+			"GIT_AUTHOR_EMAIL='%[2]s';"+
+			"GIT_COMMITTER_NAME='%[1]s';"+
+			"GIT_COMMITTER_EMAIL='%[2]s';"+
 			"HEAD",
-		}, ""), name, email))
+			name, email))
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
