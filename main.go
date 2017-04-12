84cb3c29b82587f41fdcc0e3505efaceff2a1c62
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-04-12 15:51:23 +0900

    specify from name

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
+)
+
+var (
+	fromName = flag.String("name", "", "old name")
 )
 
 func gitconfig(name string) string {
@@ -17,16 +22,21 @@ func gitconfig(name string) string {
 }
 
 func main() {
+	flag.Parse()
+	if *fromName == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
 	name := gitconfig("user.name")
 	email := gitconfig("user.email")
 	cmd := exec.Command("git", "filter-branch", "-f", "--env-filter",
-		fmt.Sprintf(""+
-			"GIT_AUTHOR_NAME='%[1]s';"+
-			"GIT_AUTHOR_EMAIL='%[2]s';"+
-			"GIT_COMMITTER_NAME='%[1]s';"+
-			"GIT_COMMITTER_EMAIL='%[2]s';"+
-			"HEAD",
-			name, email))
+		fmt.Sprintf(strings.Join([]string{
+			`if [ "$GIT_AUTHOR_NAME" = "%[1]s" ];`,
+			`then`,
+			`GIT_AUTHOR_NAME="%[2]s";`,
+			`GIT_AUTHOR_EMAIL="%[3]s";`,
+			`fi`}, "\n"),
+			*fromName, name, email), "HEAD")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
