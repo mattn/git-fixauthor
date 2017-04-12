a37210112a90e6c2dea4b2f47f182acbeda315a7
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-04-12 15:03:17 +0900

    first import

---
+++ README.md
@@ -0,0 +1,35 @@
+# git-fixauthor
+
+Have you ever committed as different git author?
+
+## Usage
+
+Don't worry, and calm down. Please drink a cup of water. Then, change the git author to real one. Next, git pull to get newest branch. And run this.
+
+```
+$ git fixauthor
+```
+
+Pray that someone else don't git push after your git pull.
+
+```
+$ git push --force
+```
+
+## Installation
+
+```
+$ go get github.com/mattn/git-fixauthor
+```
+
+## Disclaimer
+
+I will not compensate for any disadvantages that you have lost with my tool. If possible, you ask your administrator before using this.
+
+## License
+
+MIT
+
+## Author
+
+Yasuhiro Matsumoto (a.k.a. mattn)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func gitconfig(name string) string {
+	b, err := exec.Command("git", "config", name).CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.TrimSpace(string(b))
+}
+
+func main() {
+	name := gitconfig("user.name")
+	email := gitconfig("user.email")
+	cmd := exec.Command("git", "filter-branch", "-f", "--env-filter",
+		fmt.Sprintf(strings.Join([]string{
+			"GIT_AUTHOR_NAME='%[1]s';",
+			"GIT_AUTHOR_EMAIL='%[2]s';",
+			"GIT_COMMITTER_NAME='%[1]s';",
+			"GIT_COMMITTER_EMAIL='%[2]s';",
+			"HEAD",
+		}, ""), name, email))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
